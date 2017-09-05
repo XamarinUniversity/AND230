7b49b1e5b6e69c7db2fd84f815340e5ca0805ad8
Adam Patridge <patridge@gmail.com>
2017-09-05 11:34:47 -0600

    Initial course exercises import.

---
+++ Exercise 1/SHA1Extractor/DroidSHA1Extractor/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AndroidKeyStoreSHA1")]
+[assembly: AssemblyDescription("Extracts the SHA1 hash from an Android keystore - written by Adrian Stevens")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Xamarin")]
+[assembly: AssemblyProduct("AndroidKeyStoreSHA1")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2c615f9f-86d9-4b87-a162-56b92566e1c0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Exercise 3/Completed/DroidMapping/MainActivity.cs
@@ -0,0 +1,42 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Gms.Maps;
+
+namespace DroidMapping
+{
+    [Activity(Label = "DroidMapping", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity, IOnMapReadyCallback
+    {
+        GoogleMap map;
+        MapFragment mapFragment;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Find the map fragment
+            mapFragment = FragmentManager.FindFragmentById(Resource.Id.map) as MapFragment;
+
+            mapFragment.GetMapAsync(this);
+        }
+
+        public void OnMapReady(GoogleMap googleMap)
+        {
+            map = googleMap;
+
+            map.MapType = GoogleMap.MapTypeHybrid;
+            map.MyLocationEnabled = true;
+            map.UiSettings.CompassEnabled = true;
+            map.UiSettings.MyLocationButtonEnabled = true;
+            map.UiSettings.ZoomControlsEnabled = true;
+
+            //map.UiSettings.SetAllGesturesEnabled(false);
+        }
+
+    }
+}
+
